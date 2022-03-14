cdcc650468b008670ca1166e6027e9e5ea6c37c2
Mohammed-Elkhamlichi <elkhamlichimedn1et@gmail.com>
2022-03-14 22:08:05 +0100

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,13 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ["localhost", "127.0.0.1", "api.themoviedb.org", "image.tmdb.org"],
-  },
-  env: {
-    apiKey: "cc971e656b14660d19f5ced178df8863",
-    BaseApiUrl: "https://api.themoviedb.org/3/",
-  },
+    reactStrictMode: true,
+    images: {
+        domains: [
+            "localhost",
+            "127.0.0.1",
+            "api.themoviedb.org",
+            "image.tmdb.org",
+        ],
+    },
 };
 
 module.exports = nextConfig;
